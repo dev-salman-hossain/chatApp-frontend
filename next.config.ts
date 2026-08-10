b3fe9fdf55d1b3837a3955f4fff43612347c2f53
Salman472 <mds619820@gmail.com>
2026-08-11 00:21:28 +0600

    refactor(config): update app layout metadata and Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**', // এর মানে হলো Unsplash-এর যেকোনো পারথের ছবি অ্যালাউড
+      },
+    ],
+  },
 };
 
 export default nextConfig;
